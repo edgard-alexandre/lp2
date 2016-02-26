27d4291cf9baf96b51878ed4f4ab0d81eeef7f41
edgard-alexandre <edgard-alexandre@bol.com.br>
2016-02-26 08:46:09 -0300

    Added files via upload

---
+++ exercicio1/Exercicio1_3.java
@@ -0,0 +1,16 @@
+public class Exercicio1_3{
+	public static void main(String [] args){
+		int a=45,b=20,c=3;
+		int soma,mult,sub;
+		int div;
+		soma = a + b + c;
+		mult = a * b * c;
+		div = a / b / c;
+		sub = a - b - c;
+		System.out.println("O valor da soma é: " + soma);
+		System.out.println("O valor da multiplicação é: " + mult);
+		System.out.println("O valor da divisão é: " + div);
+		System.out.println("O valor da subtração é: " + sub);	
+	}
+}
+
---
+++ exercicio1/Exercicio1_4.java
@@ -0,0 +1,16 @@
+public class Exercicio1_4{
+	public static void main(String [] args){
+		float a=45,b=20,c=3;
+		float soma,mult,sub;
+		float div;
+		soma = a + b + c;
+		mult = a * b * c;
+		div = a / b / c;
+		sub = a - b - c;
+		System.out.println("O valor da soma é: " + soma);
+		System.out.println("O valor da multiplicação é: " + mult);
+		System.out.println("O valor da divisão é: " + div);
+		System.out.println("O valor da subtração é: " + sub);	
+	}
+}
+
