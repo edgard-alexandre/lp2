fd852608f562e616caf38b7773c9e6d380e03d5d
edgard-alexandre <edgard-alexandre@bol.com.br>
2016-03-09 01:30:06 -0300

    Added files via upload

---
+++ exercicio4/Palindrome.java
@@ -0,0 +1,30 @@
+public class Palindrome{
+	public static void main(String[] args){
+		String parametro = args[0];		
+		String palin;
+		String teste = parametro;
+		palin = teste;
+		int tamString,a,b,xtam,ipos,zvez=0,i,z;
+		boolean p = false;
+		xtam = teste.length();	
+		tamString = teste.length();
+		while(xtam >= 1 && p == false){				
+			a = xtam;
+			for(ipos=0; ipos <= zvez; ipos++){	
+				b = ipos + a;		
+				teste = palin.substring(ipos,b); 
+				StringBuffer sb = new StringBuffer(teste);
+				sb.reverse();
+				String reverso = sb.toString();
+				if(teste.equals(reverso)){
+					z = b - 1;
+					System.out.println(" ");
+					System.out.println(" " + teste + "  " + "Posicao: " + ipos + -+ z);
+					p = true;				
+				}
+			}
+			zvez++;
+			xtam--;
+		}
+	}
+}
